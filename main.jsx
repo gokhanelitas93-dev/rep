b11377a11c2ab1d380f2d6de356015256e5c036f
gokhanelitas93-dev <gokhan.elitas93@gmail.com>
2025-10-30 11:00:14 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AIEvreniPackGame from "./aı2.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AIEvreniPackGame />
+  </React.StrictMode>
+);
